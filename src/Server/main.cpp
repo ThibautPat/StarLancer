d9d764d7be0ffdc8462b0b8ebad73b1d98472b5a
Thib387-GC <thody@gaming.tech>
2026-01-23 14:30:00 +0100

    [FIX] COLLIISON

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -54,27 +54,7 @@ int main()
     while (true)
     {
 
-        for (User* user : network->ListUser_MainTread)
-        {
-            for (User* user1 : network->ListUser_MainTread)
-            {
-                if(user1 == user)
-					continue;   
-
-                cpu_aabb aabb1;
-                aabb1.min = user->s_EntityData->minAABB;
-                aabb1.max = user->s_EntityData->maxAABB;
-
-                cpu_aabb aabb2;
-                aabb2.min = user1->s_EntityData->minAABB;
-                aabb2.max = user1->s_EntityData->maxAABB;
-
-                if (cpu::AabbAabb(aabb1, aabb2))
-                {
-                    exit(0);
-                }
-            }
-        }
+       
 
         // PARSE
         for (const auto& message : network->MessageBufferRecev)
@@ -85,9 +65,41 @@ int main()
         EnterCriticalSection(&network->csMovedUsers);
         network->ListUser_MainTread = network->ListUser_Tread;
         LeaveCriticalSection(&network->csMovedUsers);
-
         // SEND NUKE 
         SendAllPositions(network);
+
+        for (User* user : network->ListUser_MainTread)
+        {
+            for (User* user1 : network->ListUser_MainTread)
+            {
+                if (user1 == user)
+                    continue;
+
+                cpu_aabb aabb1;
+                aabb1.min.x = user->s_EntityData->minAABB.x + user->s_EntityData->PosX;
+                aabb1.min.y = user->s_EntityData->minAABB.y + user->s_EntityData->PosY;
+                aabb1.min.z = user->s_EntityData->minAABB.z + user->s_EntityData->PosZ;
+
+                aabb1.max.x = user->s_EntityData->maxAABB.x + user->s_EntityData->PosX;
+                aabb1.max.y = user->s_EntityData->maxAABB.y + user->s_EntityData->PosY;
+                aabb1.max.z = user->s_EntityData->maxAABB.z + user->s_EntityData->PosZ;
+
+                cpu_aabb aabb2;
+                aabb2.min.x = user1->s_EntityData->minAABB.x + user1->s_EntityData->PosX;
+                aabb2.min.y = user1->s_EntityData->minAABB.y + user1->s_EntityData->PosY;
+                aabb2.min.z = user1->s_EntityData->minAABB.z + user1->s_EntityData->PosZ;
+                
+                aabb2.max.x = user1->s_EntityData->maxAABB.x + user1->s_EntityData->PosX;
+                aabb2.max.y = user1->s_EntityData->maxAABB.y + user1->s_EntityData->PosY;
+                aabb2.max.z = user1->s_EntityData->maxAABB.z + user1->s_EntityData->PosZ;
+
+                if (cpu::AabbAabb(aabb1, aabb2))
+                {
+                    exit(0);
+                }
+            }
+        }
+
 
         Sleep(10);
     }
