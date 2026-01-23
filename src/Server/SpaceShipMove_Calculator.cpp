cdc6885bfe6e0e8cbb1cd93dc2ea40b9fe27733f
Thib387-GC <thody@gaming.tech>
2026-01-23 14:03:48 +0100

    [FIX] COLLIDE

--- src/Server/SpaceShipMove_Calculator.cpp
+++ src/Server/SpaceShipMove_Calculator.cpp
@@ -5,23 +5,31 @@ void SpaceShipMove_Calculator::Calcul_Forward(User* user)
 {
     float speed = 0.1f;
     user->s_EntityData->PosZ -= speed;
+    user->s_EntityData->minAABB.z -= speed;
+	user->s_EntityData->maxAABB.z -= speed;
 }
 
 void SpaceShipMove_Calculator::Calcul_Backward(User* user)
 {
     float speed = 0.1f;
     user->s_EntityData->PosZ += speed;
+    user->s_EntityData->minAABB.z += speed;
+    user->s_EntityData->maxAABB.z += speed;
 }
 
 void SpaceShipMove_Calculator::Calcul_Left(User* user)
 {
     float speed = 0.1f;
     user->s_EntityData->PosX += speed;
+    user->s_EntityData->minAABB.x += speed;
+    user->s_EntityData->maxAABB.x += speed;
 }
 
 void SpaceShipMove_Calculator::Calcul_Right(User* user)
 {
     float speed = 0.1f;
     user->s_EntityData->PosX -= speed;
+    user->s_EntityData->minAABB.x -= speed;
+    user->s_EntityData->maxAABB.x -= speed;
 }
 
