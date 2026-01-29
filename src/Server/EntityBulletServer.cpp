22434cccd80e752eaecf8cd4b51eefbcd08cd5aa
ThibautPat <tpatry@gaming.tech>
2026-01-29 17:17:41 +0100

    Some fix

--- src/Server/EntityBulletServer.cpp
+++ src/Server/EntityBulletServer.cpp
@@ -1,2 +1,32 @@
 #include "pch.h"
 #include "EntityBulletServer.h"
+
+EntityBulletServer::EntityBulletServer()
+{
+	entityType = EntityType::BULLET;
+
+	minAABB = { -0.1f, -0.1f, -0.1f };
+	maxAABB = { 0.1f, 0.1f, 0.1f };
+}
+
+void EntityBulletServer::OnCollide(EntityServer* entity)
+{
+	IsDead = true;
+	entity->life -= damage;
+	if (entity->life < 0)
+	{
+		entity->life = 0;
+		entity->LastKiller = Owner;
+	}
+}
+
+void EntityBulletServer::Update(float dt) 
+{
+	transform.pos.x += ownerBULLET_FORWARD.x * BULLET_SPEED * dt;
+	transform.pos.y += ownerBULLET_FORWARD.y * BULLET_SPEED * dt;
+	transform.pos.z += ownerBULLET_FORWARD.z * BULLET_SPEED * dt;
+
+	LifeTime -= dt;
+	if (LifeTime <= 0)
+		IsDead = true;
+}
--- src/Server/EntityBulletServer.h
+++ src/Server/EntityBulletServer.h
@@ -9,38 +9,14 @@ public:
 
 	EntityServer* Owner;
 	XMFLOAT3 ownerBULLET_FORWARD = { 0.5f, 0.0f, 0.0f };
+
 	float LifeTime = 5;
+	const float BULLET_SPEED = 20.f;
 
-	EntityBulletServer()
-	{
-		entityType = EntityType::BULLET;
+	EntityBulletServer();
 
-		minAABB = { -0.1f, -0.1f, -0.1f };
-		maxAABB = { 0.1f, 0.1f, 0.1f };
-	}
+	void OnCollide(EntityServer* entity) override;
 
-	void OnCollide(EntityServer* entity) override
-	{
-		IsDead = true;
-		entity->life -= damage;
-		if (entity->life < 0)
-		{
-			entity->life = 0;
-			entity->LastKiller = Owner;
-		}
-	}
-
-	void Update(float dt) override
-	{
-		const float BULLET_SPEED = 20.f;
-
-		transform.pos.x += ownerBULLET_FORWARD.x * BULLET_SPEED * dt;
-		transform.pos.y += ownerBULLET_FORWARD.y * BULLET_SPEED * dt;
-		transform.pos.z += ownerBULLET_FORWARD.z * BULLET_SPEED * dt;
-
-		LifeTime -= dt;
-		if (LifeTime <= 0)
- 			IsDead = true;
-	} 
+	void Update(float dt) override;
 };
 
